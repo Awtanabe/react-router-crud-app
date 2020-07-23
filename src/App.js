6746b493b859ced7c3a4c8772f61fc4ddb720349
a_watanabe <a_watanabe@C02ZQ0UULVDP.local>
2020-07-23 20:13:47 +0900

    crudのルーティング作成

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import StreamCreate from './components/streams/StreamCreate'
 import StreamList from './components/streams/StreamList'
+import StreamCreate from './components/streams/StreamCreate'
+import StreamEdit from './components/streams/StreamEdit'
+import StreamDelete from './components/streams/StreamDelete'
+import StreamShow from './components/streams/StreamShow'
 
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={StreamList}/>
-        <Route path="/create" component={StreamCreate}/>
+        <Route path="/" exact component={StreamList}/>
+        <Route path="/streams/new" component={StreamCreate}/>
+        <Route path="/streams/edit" component={StreamEdit}/>
+        <Route path="/streams/delete" component={StreamDelete}/>
+        <Route path="/streams/show" component={StreamShow}/>
       </Router>
     </div>
   );
