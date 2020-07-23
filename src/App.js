9df957f21a34abb64a07f3df892ad2872b008eb3
a_watanabe <a_watanabe@C02ZQ0UULVDP.local>
2020-07-23 20:09:16 +0900

    crudのコンポーネント作成

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import StreamCreate from './components/streams/StreamCreate'
+import StreamList from './components/streams/StreamList'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route path="/" component={StreamList}/>
+        <Route path="/create" component={StreamCreate}/>
+      </Router>
     </div>
   );
 }
